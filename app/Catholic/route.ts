b6d0d9bc16063228806eda21a4d08cc5176c3799
tony-freedomology <tony@freedomology.com>
2026-06-29 12:34:50 -0400

    Add public Catholic briefing page

---
+++ app/Catholic/route.ts
@@ -0,0 +1,15 @@
+﻿import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+
+export const dynamic = "force-static";
+
+export async function GET() {
+  const html = await readFile(join(process.cwd(), "public", "catholic.html"), "utf8");
+
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html; charset=utf-8",
+      "cache-control": "public, max-age=300",
+    },
+  });
+}
